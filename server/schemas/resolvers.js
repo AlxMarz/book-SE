44e969bdb4b77d534819dbb5dde8cbc8a429383d
Alex Marquez <marquez798@gmail.com>
2023-08-21 23:56:25 -0500

    finshed schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,54 @@
+const { User } = require("../models");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args) => {
+      return User.find();
+    },
+    getSingleUser: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+      const correctPw = await User.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    createUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { userId, book }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $addToSet: { savedBooks: book },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+
+    removeBook: async (parent, { userId, bookId }) => {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { savedBooks: { _id: bookId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
